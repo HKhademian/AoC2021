d924359ef44ec0e1668e3f81c54fff249179b8f9
Hossain Khademian <hossain.khademian@gmail.com>
2021-12-01 00:00:00 +0330

    init AoC

--- day07/day07.kts
+++ day07/day07.kts
@@ -3,23 +3,23 @@
 import java.io.File
 
 val data = File("./input.txt")
-            .readText()
-            .replace("\n", "")
-            .split(",")
-            .map { it.toInt() }
+  .readText()
+  .replace("\n", "")
+  .split(",")
+  .map { it.toInt() }
 
 val count = data.size
 val min_pos = data.minOrNull() ?: -1
 val max_pos = data.maxOrNull() ?: -1
 
 val answer = (min_pos..max_pos)
-              .asSequence()
-              .map { target ->
-                target to data.sumOf {
-                  val dist = Math.abs(target-it)
-                  dist * (dist+1) / 2
-                }
-              }
-              .minByOrNull { it.second }
+  .asSequence()
+  .map { target ->
+    target to data.sumOf {
+      val dist = Math.abs(target-it)
+      dist * (dist+1) / 2
+    }
+  }
+  .minByOrNull { it.second }
 
 println(answer)
