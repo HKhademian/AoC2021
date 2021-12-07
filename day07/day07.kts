72db5634f85170c133941677b5cfeeeb4bda258d
Hossain Khademian <hossain.khademian@gmail.com>
2021-12-07 19:00:00 +0330

    AoC 2021 Day 07 in KTs

---
+++ day07/day07.kts
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S kotlinc -script
+
+import java.io.File
+
+val data = File("./input.txt")
+            .readText()
+            .replace("\n", "")
+            .split(",")
+            .map { it.toInt() }
+
+val count = data.size
+val min_pos = data.minOrNull() ?: -1
+val max_pos = data.maxOrNull() ?: -1
+
+val answer = (min_pos..max_pos)
+              .asSequence()
+              .map { target ->
+                target to data.sumOf {
+                  val dist = Math.abs(target-it)
+                  dist * (dist+1) / 2
+                }
+              }
+              .minByOrNull { it.second }
+
+println(answer)
